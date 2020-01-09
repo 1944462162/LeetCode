ae142d72121ebdfc921e497553025303846c5665
SuperCat <1944462162@qq.com>
2020-01-09 09:31:03 +0800

    二叉树的遍历

--- 100. 相同的树/src/Main.java
+++ 100. 相同的树/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Author: wangJianBo
  * Date: 2019/9/25 17:43
@@ -12,9 +14,48 @@ class TreeNode {
  }
 
 class Solution {
+    boolean flage = true;
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
+        hepl(p,q);
+        return flage;
+    }
+    void hepl(TreeNode root1,TreeNode root2){
+        if (root1 == null && root2 == null){
+            return;
+        }
+        if (root1 == null || root2 == null){
+            flage = false;
+            return;
+        }
+        hepl(root1.left,root2.left);
+        if (root1.val != root2.val){
+            flage = false;
+        }
+        hepl(root1.right,root2.right);
     }
 }
+
+/**
+ *            1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ */
 public class Main {
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(3);
+        treeNode.left = treeNode1;
+        treeNode.right = treeNode2;
+
+        TreeNode treeNode3 = new TreeNode(1);
+        TreeNode treeNode4 = new TreeNode(2);
+        TreeNode treeNode5 = new TreeNode(3);
+        treeNode3.left = treeNode4;
+        treeNode3.right = treeNode5;
+        Solution solution = new Solution();
+        System.out.println(solution.isSameTree(treeNode,treeNode3));
+
+    }
 }
